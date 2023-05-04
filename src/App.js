87f71f3cb462aac22f639b1a4adb048ded52b999
Renata Detros <renata.redros@gmail.com>
2023-05-04 11:12:35 -0300

    resoluç

--- src/App.js
+++ src/App.js
@@ -2,9 +2,70 @@ import Garagem from "./Componentes/Garagem";
 import "./styles.css";
 
 export default function App() {
+  function apresentaGaragem(nome) {
+  alert(`Boas vindas à garagem de ${nome}`);
+  }
+
+  const fusca = {
+    nome: "Fusca",
+    cor: "preto",
+    ano: 1972,
+    flex: "sim"
+  };
+
+  const gol = {
+    nome: "Gol",
+    cor: "verde",
+    ano: 2010,
+    flex: "não"
+  };
+
+  const limousine = {
+    nome: "Limousine",
+    cor: "branco",
+    ano: 2020,
+    flex: "não"
+  };
+
+  const carroTunado = {
+    nome: "Carro Tunado",
+    cor: "branco",
+    ano: 2020,
+    flex: "não"
+  };
+  const CarroBonito = {
+    nome: "Carro Bonito",
+    cor: "branco",
+    ano: 2020,
+    flex: "não"
+  };
+
+  const CarroMaravilhoso = {
+    nome: "Carro Maravilhoso",
+    cor: "branco",
+    ano: 2020,
+    flex: "não"
+  };
+
   return (
-    <div>
-      <Garagem />
+    <div className="App">
+      <Garagem
+        nomeGaragem="Faruqi"
+        apresentaGaragem={apresentaGaragem}
+        carro={fusca}
+        carro2={gol}
+        carro3={limousine}
+      />
+
+<Garagem
+        nomeGaragem="Garagina"
+        apresentaGaragem={apresentaGaragem}
+        carro={carroTunado}
+        carro2={CarroBonito}
+        carro3={CarroMaravilhoso}
+      />
     </div>
   );
 }
+
+//nao to precisando importar nada
--- src/Componentes/Garagem.js
+++ src/Componentes/Garagem.js
@@ -1,13 +1,14 @@
 import Carro from "./Carro";
 
-function Garagem() {
+function Garagem(props) {
+  console.log("Na garagem", props);
   return (
     <div>
-      <h1>Garagem da Labenu</h1>
-      <Carro />
-      <Carro />
-      <Carro />
-      <Carro />
+      <h1>Garagem de {props.nomeGaragem}</h1>
+      <button onClick={() => props.apresentaGaragem(props.nomeGaragem)}>Apresentar a garagem</button>
+      <Carro carro={props.carro} />
+      <Carro carro={props.carro2} />
+      <Carro carro={props.carro3}/>
     </div>
   );
 }
